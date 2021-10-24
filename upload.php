fffad2e1f792edaab895e9817abab9f670483175
e.vseev <76068702+k1lk4@users.noreply.github.com>
2021-10-24 20:50:34 +0300

    Update upload.php
    Code refactoring

--- upload.php
+++ upload.php
@@ -1,33 +1,40 @@
 <?php
-$upload_dir = 'path to files directory';
-$upload_file = basename($_FILES['uploadedFile']['name']);
+$types = [
+	"image/png" => "png",
+	"image/jpeg" => "jpg",
+	"image/gif" => "gif"
+];
+$upload_dir = './pictures/';
+$upload_file = md5(date(DATE_ATOM, time()) . basename($_FILES['uploaded_file']['name']))
+. '.' . $types[$_FILES['uploaded_file']['type']];
 $upload_path = $upload_dir . $upload_file;
 $api_url = "test-forum-api.herokuapp.com/api/file/";
 
-$types = [
-	"image/png" => "PNG",
-	"image/jpeg" => "JPEG",
-	"image/gif" => "GIF"
-];
-
 $fields = array ();
+
+$data = [];
 
 print '<pre>';
 
-if (move_uploaded_file($_FILES['uploadedFile']['tmp_name'], $upload_path)) {
+if (move_uploaded_file($_FILES['uploaded_file']['tmp_name'], $upload_path)) {
 	$fields = [
-		"extension" => $_FILES['uploadedFile']['type'],
-		"path" => $upload_path,
+		"url" => $upload_path,
 	];
+
+	$data = json_encode($fields);
 
 	$curl = curl_init($api_url);
 
 	curl_setopt($curl, CURLOPT_POST, 1);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, $fields);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
 
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($curl, CURLOPT_HEADER, false);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+			'Content-Type: application/json',
+			'Content-Length: ' . strlen($data))
+	);
 
 	$curl_result = curl_exec($curl);
 	if($curl_result === false) {
@@ -35,15 +42,17 @@ if (move_uploaded_file($_FILES['uploadedFile']['tmp_name'], $upload_path)) {
 	}
 	curl_close($curl);
 
-	print_r($curl_result);
-
-} else {
+} /*else {
 	echo "Возможная атака с помощью файловой загрузки!\n";
-}
+}*/
 
 
 echo 'Некоторая отладочная информация:';
 print_r($_FILES);
+// print_r($headers);
+
+
+print '</pre>';
 
 
 print '</pre>';
